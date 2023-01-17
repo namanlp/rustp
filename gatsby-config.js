568297dddcc480f67e2be39822dfeab1a84836f3
namanlp <naman@garudalinux.org>
2023-01-18 00:52:56 +0530

    Initial commit from gatsby: (https://github.com/gregberge/smooth-doc-starter.git)

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "smooth-doc",
+      options: {
+        name: "Smooth DOC Starter",
+        description: "Use your own description...",
+        siteUrl: "https://example.com",
+      },
+    },
+  ],
+};

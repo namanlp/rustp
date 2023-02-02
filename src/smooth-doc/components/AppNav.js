6e13f3933f8d53aca81eb1108e844d7ae40f9909
namanlp <naman@garudalinux.org>
2023-02-02 19:20:16 +0530

    Add custom header

---
+++ src/smooth-doc/components/AppNav.js
@@ -0,0 +1,44 @@
+import { RiGithubFill, RiTwitterFill } from 'react-icons/ri'
+
+import React from 'react'
+import {
+  Nav,
+  NavList,
+  NavListItem,
+  NavLink,
+  ColorModeSwitcher,
+} from 'smooth-doc/components'
+
+
+export function AppNav() {
+  return (
+    <Nav>
+      <NavList>
+          <NavListItem>
+            <NavLink
+              forwardedAs="a"
+              href="https://github.com/namanlp/rustp/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <RiGithubFill style={{ width: 24, height: 24 }} />
+            </NavLink>
+          </NavListItem>
+
+          <NavListItem>
+            <NavLink
+              forwardedAs="a"
+              href="https://twitter.com/namanlp"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <RiTwitterFill style={{ width: 24, height: 24 }} />
+            </NavLink>
+          </NavListItem>
+        <NavListItem>
+          <NavLink aria-label="Switch Theme" forwardedAs={ColorModeSwitcher} />
+        </NavListItem>
+      </NavList>
+    </Nav>
+  )
+}

b9880ad795520076258eb8d4efb4dcbecc88f366
namanlp <naman@garudalinux.org>
2023-03-13 10:34:14 +0530

    Remove algolia temporarily

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,7 @@ module.exports = {
         githubRepositoryURL: "https://github.com/namanlp/rustp",
         sections: ['Introduction', '1. Basic Programs', '2. Number Theory', '2.1 Arithmetic Operations', '2.2 Factors of a Number', '2.3 Factorials', 'Reference'],
         twitterAccount: 'namanlp',
-        docSearch: { appId: '9AFUK2OAFI', apiKey: '48cde973441f5523044add2d3185bf57', indexName: 'rustp',
-        },
+        // docSearch: { appId: '9AFUK2OAFI', apiKey: '48cde973441f5523044add2d3185bf57', indexName: 'rustp',}
       },
     },
   ],

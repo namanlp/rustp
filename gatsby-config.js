2dedd7856c567a138165feac7fad0faee01e1111
namanlp <naman@garudalinux.org>
2023-03-25 16:41:31 +0530

    Add Algolia again

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
         githubRepositoryURL: "https://github.com/namanlp/rustp",
         sections: ['Introduction', '1. Basic Programs', '2. Number Theory', '2.1 Arithmetic Operations', '2.2 Factors of a Number', '2.3 Factorials', 'Reference'],
         twitterAccount: 'namanlp',
-        docSearch: { appId: '9AFUK2OAFI', apiKey: '5e83557335cd2d4c93144517d309a790', indexName: 'rustp' },
+        docSearch: { appId: '9AFUK2OAFI', apiKey: '5e83557335cd2d4c93144517d309a790', indexName: 'rustp'   },
       },
     },
   ],

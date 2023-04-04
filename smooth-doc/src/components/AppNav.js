6cd80dc1da38970d6510a952ff1b92aa8e216d92
namanlp <naman@garudalinux.org>
2023-04-05 01:52:46 +0530

    Replace book with default react icon

--- smooth-doc/src/components/AppNav.js
+++ smooth-doc/src/components/AppNav.js
@@ -5,8 +5,6 @@ import React from 'react'
 import { Nav, NavList, NavListItem, NavLink } from './Nav'
 import { ColorModeSwitcher } from './ColorModeSwitcher'
 
-import "./AppNavStyle.css"
-
 export function AppNav() {
     return (
         <Nav>

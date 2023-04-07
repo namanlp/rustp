39283960f472fb708988785d3c6269a488ebd6c7
namanlp <naman@garudalinux.org>
2023-04-07 23:45:43 +0530

    Change analytics to anonymous

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -19,15 +19,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
         trackingId: "G-8B0V483RV8",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        // head: false,
-        // Setting this parameter is optional
-        // anonymize: true,
-        // Avoids sending pageview hits from custom paths
+        anonymize: true,
         // exclude: ["/home"],
-        // defer: true,
+        defer: true,
       },
     },
   ],

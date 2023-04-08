17a6f6265b061a23cf2fb326d9c57a464c5da24f
namanlp <naman@garudalinux.org>
2023-04-09 01:39:07 +0530

    Clean up Gatsby config

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -20,24 +20,17 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: [
-          "G-D0G3P087MQ", // Google Analytics / GA
+          "G-D0G3P087MQ", // Google Analytics
         ],
-        // This config will be shared across all trackingIds
         gtagConfig: {
           // optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           // cookie_expires: 0,
         },
-        // This object is used for configuration specific to this plugin
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
           head: true,
-          // Setting this parameter is also optional
           respectDNT: true,
-          // Avoids sending pageview hits from custom paths
           // exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          // origin: "YOUR_SELF_HOSTED_ORIGIN",
           // Delays processing pageview events on route update (in milliseconds)
           delayOnRouteUpdate: 0,
         },

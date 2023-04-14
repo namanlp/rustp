ebf9fe5bce6e3ac5faca1a88e75fd6153a0315c6
namanlp <naman@garudalinux.org>
2023-04-14 19:30:48 +0530

    Reorganize pages

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
         siteUrl: "https://rustp.org",
         baseDirectory: path.resolve(__dirname, '../'),
         author: "Naman Garg",
-        sections: ['Introduction', '1. Basic Programs', '2. Number Theory', '2.1 Arithmetic Operations', '2.2 Factors of a Number', '2.3 Factorials', 'Reference'],
+        sections: ['1. Basic Programs', '2. Number Theory', '2.1 Arithmetic Operations', '2.2 Factors of a Number', '3 Combinatorics'],
         twitterAccount: 'namanlp',
         githubRepositoryURL: 'https://github.com/namanlp/rustp/',
         docSearch: { appId: '9AFUK2OAFI', apiKey: '5e83557335cd2d4c93144517d309a790', indexName: 'rustp'   },

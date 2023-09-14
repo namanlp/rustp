830ea6a8a2389dfa26d5fb1bb2198ad1169b6a5f
namanlp <naman@garudalinux.org>
2023-09-14 18:49:02 +0530

    Add new twitter handle

--- smooth-doc/src/components/AppNav.js
+++ smooth-doc/src/components/AppNav.js
@@ -1,4 +1,4 @@
-import { RiGithubFill } from 'react-icons/ri'
+import { RiGithubFill, RiTwitterFill } from 'react-icons/ri'
 import {FaBook} from "react-icons/fa";
 
 import React from 'react'
@@ -32,6 +32,19 @@ export function AppNav() {
                         <RiGithubFill style={{ width: 24, height: 24 }} />
                     </NavLink>
                 </NavListItem>
+
+                <NavListItem>
+                    <NavLink
+                        forwardedAs="a"
+                        href="https://twitter.com/rustp_official"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Follow us on Twitter"
+                    >
+                        <RiTwitterFill style={{ width: 24, height: 24 }} />
+                    </NavLink>
+                </NavListItem>
+
                 <NavListItem>
                     <NavLink aria-label="Switch Theme" as={ColorModeSwitcher} />
                 </NavListItem>
